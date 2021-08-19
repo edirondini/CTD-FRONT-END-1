90a0d5878dcbe4ee51b7b2483cb31515eeb40c08
edirondini <59415087+edirondini@users.noreply.github.com>
2021-08-18 21:32:37 -0300

    COMMIT INICIAL

---
+++ FRONT END I/JAVASCRIPT/tiposDeDados.js
@@ -0,0 +1,11 @@
+    //objeto literal
+    let moto = {
+        marca: "Honda",
+        ano: "2019",
+        modelo: "CB500"
+    }
+    console.log(moto);
+
+    //array
+    let series = ["Friends", "Todo mundo odeia o Cris", "Arnold"];
+    console.log (series);
